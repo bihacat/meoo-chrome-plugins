8a13b6283c163f6b6c8ba9e894e35f5ce570b45d
bihacat <bihacats@gmail.com>
2021-03-21 14:54:29 +0800

    feat:icourse163自动答题挂视频脚本

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
   entry: {
     chaoxing: "./src/chaoxing/main.js",
     mosoteach: './src/mosoteach/main.js',
-    nfstar: './src/nfstar/main.js'
+    nfstar: './src/nfstar/main.js',
+    icourse163: './src/icourse163/main.js'
   },
   output: {
     filename: '[name]/main.js',
