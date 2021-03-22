a22fdd1a1b700f2efc546d7e0b5aab3b074dca7c
bihacats <bihacats@gmail.com>
2021-03-22 15:36:09 +0800

    修复有时无法获取video的问题

--- src/icourse163/main.js
+++ src/icourse163/main.js
@@ -1,32 +1,39 @@
-import $ from 'jquery'
-
 let answerTime = 0;
-$(document).ready(function () {
-  try {
-    mainScript()
-  } catch (e) {
-    console.log('捕获异常, 1秒后重新尝试', e)
-    setTimeout(mainScript, 1000)
+document.onreadystatechange = function () {
+  switch (document.readyState) {
+    case "complete":
+      mainScript()
+      break;
   }
-})
+}
 
 function mainScript() {
-  const video = $('video')
-  video.onpause = function () {
-    // 点击一个选项
-    document.querySelector('').children[answerTime].querySelector('input').click()
-    // 点击
-    document.querySelector('.u-btn.u-btn-default.submit.j-submit').click()
-    const answerDom = document.querySelector('.analysisInfo')
-    const result = answerDom.attributes.class.textContent.split(' ').includes('answrong')
-    if (result) {
-      console.log('回答错误，视频回退5秒，重新答题')
-      video.currentTime -= 5;
-      answerTime += 1
-    } else {
-      console.log('回答正确，继续看视频', answerDom)
-      answerTime = 0
-    }
-    $('.u-btn.u-btn-default.cont.j-continue').click()
+  const video = document.querySelector('video')
+  video.onpause = () => autoAnswer(video)
+}
+
+function autoAnswer(video) {
+  // 点击一个选项
+  console.log('paused')
+  const answerContainer = document.querySelector('.choices.f-cb')
+  if (!answerContainer) {
+    console.log('获取答题区域失败，1秒后重新获取');
+    setTimeout(autoAnswer, 1000);
+    return
+  };
+
+  answerContainer.children[answerTime].querySelector('input').click()
+  // 点击
+  document.querySelector('.u-btn.u-btn-default.submit.j-submit').click()
+  const answerDom = document.querySelector('.analysisInfo')
+  const result = answerDom.attributes.class.textContent.split(' ').includes('answrong')
+  if (result) {
+    console.log('回答错误，视频回退5秒，重新答题')
+    video.currentTime -= 5;
+    answerTime += 1
+  } else {
+    console.log('回答正确，继续看视频', answerDom)
+    answerTime = 0
   }
+  document.querySelector('.u-btn.u-btn-default.cont.j-continue').click()
 }
